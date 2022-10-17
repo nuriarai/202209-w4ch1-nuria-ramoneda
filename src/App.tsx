ac86d8b297c320ec817922866d3afc67fc5922f9
Núria Ramoneda <nuriarai@gmail.com>
2022-10-17 19:40:01 +0200

    Add button component and its css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <h1>Señoros React</h1>;
+  return (
+    <>
+      <h1>Señoros React</h1>
+      <Button />
+    </>
+  );
 }
 
 export default App;
